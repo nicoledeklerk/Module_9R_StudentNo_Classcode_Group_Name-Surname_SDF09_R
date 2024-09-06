7fd771d2976b8538843143d43e04603ff0bb94fc
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2024-09-06 11:45:16 +0200

    Create getRandomCard function
    Created a function that renders random cards between 1 - 13

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -8,6 +8,17 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
     renderGame()
@@ -35,7 +46,7 @@ function renderGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!")
-    let card = 6
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     renderGame()
