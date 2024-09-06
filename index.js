91b037e5d46390381adab60ddb9ce12935f8b6b3
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2024-09-06 12:49:39 +0200

    Fix renderGame function
    Added missing curly bracket to end of renderGame function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+console.log(cards)
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
@@ -21,15 +21,20 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
-
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
+  
     
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -44,8 +49,8 @@ function renderGame() {
     messageEl.textContent = message
 }
 
+
 function newCard() {
-    console.log("Drawing a new card from the deck!")
     let card = getRandomCard()
     sum += card
     cards.push(card)
